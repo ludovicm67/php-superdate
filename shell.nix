0cad17ce12925a75ce5b2aa90bfade07b7515b98
Ludovic Muller <ludovicmuller1@gmail.com>
2025-05-29 12:10:54 +0200

    chore: upgrade phpunit to v12 and require calendar

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  php = pkgs.php84.buildEnv {
+    extensions = { enabled, all }:
+      with all; [
+        xdebug
+        dom
+        mbstring
+        tokenizer
+        xmlwriter
+        simplexml
+        filter
+        openssl
+        ctype
+        curl
+
+        calendar
+      ];
+  };
+
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ git php php.packages.composer ];
+
+  shellHook = ''
+    export XDEBUG_MODE=coverage
+  '';
+}
